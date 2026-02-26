055215d7494bd119e851aeae99a4acb982620ff6
감성은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2026-02-26 17:15:35 +0900

    lotto.py 업데이트

--- lotto.py
+++ lotto.py
@@ -1,6 +1,11 @@
 import random as ran
-
-for i in range(6)
-  ran.radint(1,45)
-
-print("로또 추첨 번호 :"{i})
+import time
+lotto = ran.sample(range(1, 46), 6)
+time.sleep(1)
+print("강현쌤을 위한 로또추첨기!!.ver1")
+time.sleep(1)
+print("강현쌤은 부자가 될것인가?")
+time.sleep(1)
+print("두구두구두구두구두구")
+time.sleeep(3)
+print("로또 추첨 번호 :", lotto)
